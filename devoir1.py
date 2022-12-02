5c9c0758528d55cd13c9be4b0e9c807b74242279
Beths21 <bethsvaldemar@gmail.com>
2022-12-02 13:58:07 -0500

    modification

--- devoir1.py
+++ devoir1.py
@@ -5,14 +5,14 @@ from random import randrange
 jwe = 1
 while jwe == 1:
     chans = 5
-    nonb_odinate = randrange(0,500) 
+    nonb_odinate = randrange(0,500)
+    print("--------------------------------woulet------------------------------------")
+    print("____________jwe_____________")
+    print("**************************************************************************") 
     while chans > 0:
-        print("--------------------------------woulet------------------------------------")
-        print("____________jwe_____________")
-        print("**************************************************************************")
         print("rete ", chans, " chans \n")
         print("ou dwe chwazi ant 0 a 500\n")
-        vale_chwazi = int(input("ki nomb ou chwazi? "))
+        vale_chwazi = int(input("ki nomb ou chwazi? \n"))
 
         if vale_chwazi < nonb_odinate:
             print("vale a tro piti!") 
@@ -25,7 +25,8 @@ while jwe == 1:
         chans -= 1
     if chans == 0:
         print("ou pedi!\n")
-        print(f"vale a se: {nonb_odinate}")
+        print(f"vale a se: {nonb_odinate}\n")
         jwe = int(input("siw vle kontinye jwe , peze 1: "))
+        print("^^^^^^^^^^^^^^^^^^%^^^^^^^^^^^^^^^^^^^^^^^^")
 
 
